56c744807bca30de3c5dc170c924b650789e8bbe
laurence13 <57928149+laurence13@users.noreply.github.com>
2020-03-03 16:42:14 +0800

    Add files via upload

---
+++ Scripts/ClicktoDestroy.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ClicktoDestroy : MonoBehaviour
+{
+    void OnMouseDown()
+    {
+        Destroy (gameObject);
+
+    }
+}
---
+++ Scripts/Playbutton.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class Playbutton : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public void SceneLoader(int SceneIndex)
+    {
+        SceneManager.LoadScene (SceneIndex);
+    }
+
+}
